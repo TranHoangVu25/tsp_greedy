ab01a9dcdf865bae7cf7f9a36b5d06c7b0c6280f
Tran_Hoang_Vu <hoangvudx2004@gmail.com>
2024-12-22 12:21:32 +0700

    update

--- src/tsp_greedy.py
+++ src/tsp_greedy.py
@@ -4,6 +4,54 @@ import matplotlib.pyplot as plt
 
 INT_MAX = 2147483647
 
+# def findMinRoute(tsp):
+#     total_distance = 0
+#     counter = 0
+#     j = 0
+#     i = 0
+#     min_distance = INT_MAX
+#     visitedRouteList = DefaultDict(int)
+
+#     visitedRouteList[0] = 1
+#     route = [0] * len(tsp)
+
+#     # Danh sách lưu các thành phố đã đi qua
+#     cities_visited = [0]  # Bắt đầu từ thành phố 0
+
+#     while i < len(tsp) and j < len(tsp[i]):
+#         if counter >= len(tsp[i]) - 1:
+#             break
+#         if j != i and (visitedRouteList[j] == 0):
+#             if tsp[i][j] < min_distance:
+#                 min_distance = tsp[i][j]
+#                 route[counter] = j + 1
+#         j += 1
+#         if j == len(tsp[i]):
+#             total_distance += min_distance
+#             min_distance = INT_MAX
+#             visitedRouteList[route[counter] - 1] = 1
+#             cities_visited.append(route[counter] - 1)  # Lưu thành phố đã đi qua
+#             j = 0
+#             i = route[counter] - 1
+#             counter += 1
+
+#     i = route[counter - 1] - 1
+
+#     for j in range(len(tsp)):
+#         if (i != j) and tsp[i][j] < min_distance:
+#             min_distance = tsp[i][j]
+#             route[counter] = j + 1
+
+#     total_distance += min_distance
+#     cities_visited.append(route[counter] - 1)  # Lưu thành phố cuối cùng
+
+#     # In kết quả
+#     print("Minimum Cost is:", total_distance)
+#     print("Route taken by the truck:", " -> ".join(map(str, cities_visited)))
+
+#     return cities_visited
+
+
 def findMinRoute(tsp):
     total_distance = 0
     counter = 0
@@ -12,38 +60,33 @@ def findMinRoute(tsp):
     min_distance = INT_MAX
     visitedRouteList = DefaultDict(int)
 
-    visitedRouteList[0] = 1
+    visitedRouteList[0] = 1  # Đánh dấu thành phố đầu tiên đã được ghé thăm
     route = [0] * len(tsp)
 
     # Danh sách lưu các thành phố đã đi qua
     cities_visited = [0]  # Bắt đầu từ thành phố 0
 
-    while i < len(tsp) and j < len(tsp[i]):
-        if counter >= len(tsp[i]) - 1:
-            break
-        if j != i and (visitedRouteList[j] == 0):
-            if tsp[i][j] < min_distance:
+    while counter < len(tsp) - 1:
+        min_distance = INT_MAX
+        next_city = -1
+
+        # Tìm thành phố gần nhất chưa được ghé thăm
+        for j in range(len(tsp)):
+            if j != i and visitedRouteList[j] == 0 and tsp[i][j] < min_distance:
                 min_distance = tsp[i][j]
-                route[counter] = j + 1
-        j += 1
-        if j == len(tsp[i]):
-            total_distance += min_distance
-            min_distance = INT_MAX
-            visitedRouteList[route[counter] - 1] = 1
-            cities_visited.append(route[counter] - 1)  # Lưu thành phố đã đi qua
-            j = 0
-            i = route[counter] - 1
+                next_city = j
+
+        # Cập nhật thông tin
+        if next_city != -1:
+            visitedRouteList[next_city] = 1  # Đánh dấu thành phố đã ghé thăm
+            cities_visited.append(next_city)  # Lưu thành phố vào lộ trình
+            total_distance += min_distance  # Cộng khoảng cách
+            i = next_city  # Chuyển sang thành phố tiếp theo
             counter += 1
 
-    i = route[counter - 1] - 1
-
-    for j in range(len(tsp)):
-        if (i != j) and tsp[i][j] < min_distance:
-            min_distance = tsp[i][j]
-            route[counter] = j + 1
-
-    total_distance += min_distance
-    cities_visited.append(route[counter] - 1)  # Lưu thành phố cuối cùng
+    # Quay về thành phố đầu tiên
+    total_distance += tsp[i][0]
+    cities_visited.append(0)  # Quay lại thành phố đầu tiên
 
     # In kết quả
     print("Minimum Cost is:", total_distance)
@@ -83,7 +126,7 @@ def plot_route(data, route):
             c='red', linewidth=1.5
         )
 	
-    # Vẽ đường quay lại thành phố đầu tiên (nét đậm hơn)
+  
     city_last = route[-1]
     city_start = route[0]
     plt.plot(
@@ -110,7 +153,7 @@ def plot_route(data, route):
 # 	findMinRoute(tsp)
 # main()
 def main():
-    file_path = "D:\\Tran Hoang Vu\\Semester 5\\Artificical intelligence\\final assigment\\tsp_greedy\\data\\15.txt"
+    file_path = "D:\\Tran Hoang Vu\\Semester 5\\Artificical intelligence\\final assigment\\tsp_greedy\\data\\26.txt"
     data = np.loadtxt(file_path, dtype=int)
 
     tsp = np.zeros((len(data), len(data)), dtype=int)
